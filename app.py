1d31c78d72e2ec2a0c5c73d62847ed598c0bc8f0
or <orzeharia85@gmail.com>
2022-06-17 20:09:17 +0300

    HW 3

--- app.py
+++ app.py
@@ -100,28 +100,49 @@ def assignment3_1_func():
         hobbie = request.args.get('user_hob', "")
         if hobbie == '':
             return render_template('assignment3_1.html', hobbies_dict=hobbies_dict, social_li=social_li,
-                                   alone_li=alone_li, message='think')
+                                   alone_li=alone_li, message='You did not wrote a hobby')
         for key in hobbies_dict:
             if hobbie.upper() == key.upper():
                 session['user_hobbie'] = hobbie
                 session['have_hobbie'] = True
-                return render_template('assignment3_1.html', hobbies_dict=hobbies_dict, social_li=social_li, alone_li=alone_li, message='you choose somthing from the list')
+                session['hobbieFromList'] = True
+                return render_template('assignment3_1.html', hobbies_dict=hobbies_dict, social_li=social_li, alone_li=alone_li, message='you choose something from the list')
 
             else:
+
                 session['user_hobbie'] = hobbie
                 session['have_hobbie'] = True
                 return render_template('assignment3_1.html', hobbies_dict=hobbies_dict, social_li=social_li,
-                                       alone_li=alone_li, message='new')
+                                       alone_li=alone_li, message='The hobby you write is not on the list, interesting!')
 
     else:
         return render_template('assignment3_1.html', hobbies_dict=hobbies_dict, social_li=social_li, alone_li=alone_li)
 
 
+@app.route('/assignment3_1b')
+def assignment3_1b_func():
+    social_li.clear()
+    alone_li.clear()
+    if 'hob_type' in request.args:
+        type = request.args.get('hob_type', "")
+        if type.lower()=='social':
+            social_li.append(session['user_hobbie'])
+            return render_template('assignment3_1.html', hobbies_dict=hobbies_dict, social_li=social_li,
+                                   alone_li=alone_li, message='insert social list')
+        if type.lower()=='alone':
+            alone_li.append(session['user_hobbie'])
+            return render_template('assignment3_1.html', hobbies_dict=hobbies_dict, social_li=social_li,
+                                   alone_li=alone_li, message='insert alone list')
+        if type.lower()!='social' and type.lower()!='alone':
+            return render_template('assignment3_1.html', hobbies_dict=hobbies_dict, social_li=social_li,
+                                   alone_li=alone_li, message='You did not make the right choice, choose social or alone')
+    return render_template('assignment3_1.html', hobbies_dict=hobbies_dict, social_li=social_li, alone_li=alone_li)
 
 @app.route('/delete_hobbie')
 def delete_func():
     session['have_hobbie'] = False
     session['user_hobbie']=""
+    session['hobbieFromList'] = False
     social_li.clear()
     alone_li.clear()
     return redirect(url_for('assignment3_1_func'))
